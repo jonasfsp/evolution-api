75e66c8386f177aa1fdcbd3049fa8537e3b88caa
Jonas Fernando <36555257+jonasfsp@users.noreply.github.com>
2023-09-02 09:35:46 -0300

    Update env.config.ts

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -154,7 +154,7 @@ export class ConfigService {
     this.env.PRODUCTION = process.env?.NODE_ENV === 'PROD';
     if (process.env?.DOCKER_ENV === 'true') {
       this.env.SERVER.TYPE = 'http';
-      this.env.SERVER.PORT = 8080;
+      this.env.SERVER.PORT = 8083;
     }
   }
 
